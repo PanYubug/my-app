1f469f023f1d222eb691b4008bd5df7e6a146be2
panyu <344146473@qq.com>
2021-04-19 00:14:38 +0800

    8\9\10\11

---
+++ 09_src_NavLink的使用/App.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+import { NavLink, Route } from 'react-router-dom';
+import Header from './components/Header'; //Header是一般组件
+import About from './pages/About'; // About是路由组件
+import Home from './pages/Home';// Home是路由组件
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <div className="row">
+          <div className="col-xs-offset-2 col-xs-8">
+            <Header a={1}/>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-xs-2 col-xs-offset-2">
+            <div className="list-group">
+              {/* 原生html中，靠<a>跳转不同的页面 */}
+              {/* <a className="list-group-item active" href="./about.html">About</a>
+              <a className="list-group-item" href="./home.html">Home</a> */}
+              {/* 在React中靠路由连接实现切换组件---编写路由连接 */}
+              <NavLink activeClassName="atguigu" className="list-group-item" to="/about">About</NavLink>
+              <NavLink activeClassName="atguigu" className="list-group-item" to="/home">Home</NavLink>
+            </div>
+          </div>
+          <div className="col-xs-6">
+            <div className="panel">
+              <div className="panel-body">
+                {/* 注册路由 */}
+                <Route path="/about" component={About} />
+                <Route path="/home" component={Home} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
