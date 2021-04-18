ce4afbf63587c1a01ab13274459e8b3c8a33d6ec
panyu <344146473@qq.com>
2021-04-19 00:12:18 +0800

    8\9\10\11

--- README.md
+++ README.md
@@ -1,7 +1,35 @@
-
-
-
-
+三、路由的基本使用
+四、路由组件与一般组件
+1、写法不同
+  一般组件：<Demo />
+  路由组件：<Route path="/about" component={About} />
+2、存放位置不同
+  一般组件：components
+  路由组件：pages
+3、接收到的props不同：
+  一般组件：写组件标签时传递了什么，就能收到什么
+  路由组件：接收到三个固定的属性
+          history:
+            go:ƒ go() {}
+            goBack:ƒ goBack() {}
+            goForward:ƒ goForward() {}
+            push:ƒ push() {}
+            replace:ƒ replace() {}
+          location:
+            pathname:"/about"
+            search:""
+            state:undefined
+          match:
+            params:{}
+            path:"/"
+            url:"/"
+五、NavLink与封装NavLink
+  1、NavLink可以实现路由链接的高亮，通过activeClassName指定样式名
+  2、标签体内容时一个特殊的标签属性
+  3、通过this.props.children可以获取标签体内容
+六、Switch的使用
+  1、通常情况下，path和component是一一对应的关系
+  2、Switch可以提高路由匹配效率（单一匹配）
 
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,44 @@
 import React, { Component } from 'react'
+import { Route, Switch } from 'react-router-dom';
+import Header from './components/Header'; //Header是一般组件
+import MyNavLink from './components/MyNavLink';
+import TestD from './components/TestD';
+import About from './pages/About'; // About是路由组件
+import Home from './pages/Home';// Home是路由组件
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        App...
+        <div className="row">
+          <div className="col-xs-offset-2 col-xs-8">
+            <Header a={1} />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-xs-2 col-xs-offset-2">
+            <div className="list-group">
+              {/* 原生html中，靠<a>跳转不同的页面 */}
+              {/* <a className="list-group-item active" href="./about.html">About</a>
+              <a className="list-group-item" href="./home.html">Home</a> */}
+              {/* 在React中靠路由连接实现切换组件---编写路由连接 */}
+              <MyNavLink to="/about">About</MyNavLink>
+              <MyNavLink to="/home">Home</MyNavLink>
+            </div>
+          </div>
+          <div className="col-xs-6"> 
+            <div className="panel">
+              <div className="panel-body">
+                {/* 注册路由 */}
+                <Switch>
+                <Route path="/about" component={About} />
+                <Route path="/home" component={Home} />
+                <Route path="/home" component={TestD} />
+                </Switch>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
