a915642f58cda26b8f24558ef954324080d60262
panyu <344146473@qq.com>
2021-04-20 23:12:13 +0800

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { Button, DatePicker } from 'antd';
-import 'antd/dist/antd.css'
+import '../22_src_Antd使用/node_modules/antd/dist/antd.css'
 import { WechatOutlined, SearchOutlined } from '@ant-design/icons';
 
 const { RangePicker } = DatePicker;
@@ -15,13 +15,6 @@ export default class App extends Component {
     return (
       <div>
         App...
-        <Button type="primary">按钮1</Button>
-        <Button >按钮2</Button>
-        <Button type="link">按钮3</Button>
-        <WechatOutlined />
-        <Button type="primary" icon={<SearchOutlined />}>Search</Button>
-        <DatePicker onChange={onChange} />
-        <RangePicker />
       </div>
     )
   }
